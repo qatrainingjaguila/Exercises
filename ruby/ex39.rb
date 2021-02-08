b38d285d4d051ee82bf838a8fd3eaede97e7f7ae
Juan A <jaguila@academytrainee.com>
2021-02-08 00:00:34 +0000

    completed ruby 39

--- ruby/ex39.rb
+++ ruby/ex39.rb
@@ -15,3 +15,23 @@ cities = {
 cities['NY']='New York'
 cities['OR']='Portland'
 
+puts '-' * 10
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
+
+puts '-' * 10
+puts "Michigan's abbreviation is: #{states['Michigan']}"
+
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+
+puts '-' * 10
+cities.each do |abbrev,city|
+    puts "#{abbrev} has the city #{city}"
+end
+
+puts '-' * 10
+states.each do |state,abbrev|
+    city = cities[abbrev]
+    puts = "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
